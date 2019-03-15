5ee0b340e8b4b848b563051644c6d08d09fbf449
William Duplenne <will.dupl95@gmail.com>
2019-03-15 14:17:42 +0100

    add useContext

--- src/containers/Playground/index.js
+++ src/containers/Playground/index.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { createContext } from 'react';
 
 import BasicHooks from '../../components/Hooks/BasicHooks';
 import EffectHooks from '../../components/Hooks/EffectHooks';
+import ContextHooks from '../../components/Hooks/ContextHooks';
 
 import './index.scss';
 
+export const ThemeContext = createContext();
+
 const Playground = () => (
-  <header className="app-header">
-    <div className="app-block">
-      <p>BasicHooks:</p>
-      <BasicHooks />
-    </div>
-    <div className="app-block">
-      <p>EffectHooks:</p>
-      <EffectHooks />
-    </div>
-  </header>
+  <ThemeContext.Provider value="blue">
+    <header className="app-header">
+      <div className="app-block">
+        <p>BasicHooks:</p>
+        <BasicHooks />
+      </div>
+      <div className="app-block">
+        <p>EffectHooks:</p>
+        <EffectHooks />
+      </div>
+      <div className="app-block">
+        <p>ContextHooks:</p>
+        <ContextHooks />
+      </div>
+    </header>
+  </ThemeContext.Provider>
 );
 
 export default Playground;
