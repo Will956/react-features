4207afbf0df1cddc0987e49345af0a6796517eeb
William Duplenne <will.dupl95@gmail.com>
2020-04-19 18:38:12 +0200

    :art: New CRA start

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import Playground from './containers/ErrorBoundaries';
-
-ReactDOM.render(<Playground />, document.getElementById('root'));
